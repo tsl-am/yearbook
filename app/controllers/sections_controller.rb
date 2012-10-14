d804f53afc87de1e96f310591924f7698162b7f4
Raghuveera Betina <rbetina@gmail.com>
2012-10-14 00:16:01 -0500

    Explore API data.

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -3,5 +3,6 @@ require 'open-uri'
 class SectionsController < ApplicationController
   def am_section    
     @response = open("http://codemountain.com/api/students/am.json").read
+    @parsed_data = JSON.parse(@response)
   end
 end
