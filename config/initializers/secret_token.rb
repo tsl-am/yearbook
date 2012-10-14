91e63c68121533f6f8f16df06032e9c412b53b30
Raghuveera Betina <rbetina@gmail.com>
2012-10-13 22:12:15 -0500

    rails new Yearbook

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yearbook::Application.config.secret_token = 'c18742229dc983f2a00c43336eeede19f2a7d37d4c7d3a9e370bfc4b2dde87a40a15eff527478e94999c908242e34c2d2e1a245276254b23cb20df7e5117f56f'
