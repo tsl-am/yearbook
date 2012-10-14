60db59f3870d4809f95e6b96a0d4bc8e549eca7c
Raghuveera Betina <rbetina@gmail.com>
2012-10-14 00:20:10 -0500

    Utilize API data to draw list.

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -3,6 +3,6 @@ require 'open-uri'
 class SectionsController < ApplicationController
   def am_section    
     @response = open("http://codemountain.com/api/students/am.json").read
-    @parsed_data = JSON.parse(@response)
+    @students = JSON.parse(@response)
   end
 end
