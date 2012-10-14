3d167e0c9c2a44d5a57270a0d4bfed19209fd62a
Raghuveera Betina <rbetina@gmail.com>
2012-10-14 00:27:06 -0500

    C, A for PM section.

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -5,4 +5,9 @@ class SectionsController < ApplicationController
     @response = open("http://codemountain.com/api/students/am.json").read
     @students = JSON.parse(@response)
   end
+  
+  def pm_section    
+    @response = open("http://codemountain.com/api/students/pm.json").read
+    @students = JSON.parse(@response)
+  end
 end
