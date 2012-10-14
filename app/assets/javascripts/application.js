a663d62fca57916ec274ed5c539031acf373bcbc
Raghuveera Betina <rbetina@gmail.com>
2012-10-14 00:37:17 -0500

    For fun: some jquery. We'll learn it soon.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,9 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+$(document).ready(function() {
+  $('.thumbnail').on('click', function() {
+    $(this).siblings().fadeToggle();
+  });
+});
